711f255522a6e5daf2c8a0424927a3af17773c65
NI210248 <NI210248@reigate.ac.uk>
2021-09-28 16:12:20 +0100

    save 2

--- src/com/company/FiveNumTotal.java
+++ src/com/company/FiveNumTotal.java
@@ -1,7 +1,11 @@
 package com.company;
 
-public class FiveNumTotal {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class FiveNumTotal {
+    public static void five() {
+        Scanner input = new Scanner(System.in);
+        Integer num1 = input.nextInt();
+        System.out.println(num1);
     }
 }
